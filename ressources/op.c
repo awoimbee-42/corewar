64e15088c5a5ca85aa125ceadc3d37c6da8bb06e
Arthur WOIMBEE <awoimbee@e2r11p12.42.fr>
2019-05-02 14:50:45 +0200

    First commit, add project ressources (including op.[hc], containing the info we gathered about the project), let s code!

---
+++ ressources/op.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/10/04 11:43:01 by zaz               #+#    #+#             */
+/*   Updated: 2019/05/02 14:48:20 by awoimbee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "op.h"
+
+typedef enum	e_bool
+{
+	FALSE,
+	TRUE
+}				t_bool;
+
+typedef struct	s_op
+{
+	char		*name;
+	int			nb_args;
+	t_arg_type	args_types[3]; // 3 is the max number of args -> the compiler will round to 4 anyways
+	int			id; //op_code
+	int			cycles;
+	char		*desc;
+	t_bool		coding_byte;
+	t_bool		carry;
+}				t_op;
+
+
+t_op    op_tab[17] =
+{
+//  name - nb_args -- args_types --                                 id -- nb_cycles -- description --      uses_coding_byte -- uses_carry
+	{"live", 1, {T_DIR},                                                1,   10, "alive",                                0, 0},
+	{"ld",   2, {T_DIR | T_IND, T_REG},                                 2,    5, "load",                                 1, 0},
+	{"st",   2, {T_REG, T_IND | T_REG},                                 3,    5, "store",                                1, 0},
+	{"add",  3, {T_REG, T_REG, T_REG},                                  4,   10, "addition",                             1, 0},
+	{"sub",  3, {T_REG, T_REG, T_REG},                                  5,   10, "soustraction",                         1, 0},
+	{"and",  3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG},  6,    6, "et (and  r1, r2, r3   r1&r2 -> r3",    1, 0},
+	{"or",   3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},  7,    6, "ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0},
+	{"xor",  3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},  8,    6, "ou (xor  r1, r2, r3   r1^r2 -> r3",    1, 0},
+	{"zjmp", 1, {T_DIR},                                                9,   20, "jump if zero",                         0, 1},
+	{"ldi",  3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG},         10,   25, "load index",                           1, 1},
+	{"sti",  3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG},         11,   25, "store index",                          1, 1},
+	{"fork", 1, {T_DIR},                                               12,  800, "fork",                                 0, 1},
+	{"lld",  2, {T_DIR | T_IND, T_REG},                                13,   10, "long load",                            1, 0},
+	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG},         14,   50, "long load index",                      1, 1},
+	{"lfork",1, {T_DIR},                                               15, 1000, "long fork",                            0, 1},
+	{"aff",  1, {T_REG},                                               16,    2, "aff",                                  1, 0},
+	{0, 0, {0}, 0, 0, 0, 0, 0}
+};
---
+++ ressources/op.h
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
+/*   Updated: 2019/05/02 14:45:31 by awoimbee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Toutes les tailles sont en octets.
+** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
+*/
+
+/*
+**	Sizes of objects
+*/
+
+#define IND_SIZE				2
+#define REG_SIZE				4
+#define DIR_SIZE				REG_SIZE
+
+/*
+**
+*/
+
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
+
+
+#define MAX_ARGS_NUMBER			4
+#define MAX_PLAYERS				4
+#define MEM_SIZE				(4*1024)
+#define IDX_MOD					(MEM_SIZE / 8)
+#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+
+#define COMMENT_CHAR			'#'
+#define LABEL_CHAR				':'
+#define DIRECT_CHAR				'%' // 34 == direct, %34 == indirect
+#define SEPARATOR_CHAR			','
+
+#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+
+#define NAME_CMD_STRING			".name"
+#define COMMENT_CMD_STRING		".comment"
+
+#define REG_NUMBER				16
+
+#define CYCLE_TO_DIE			1536
+#define CYCLE_DELTA				50
+#define NBR_LIVE				21
+#define MAX_CHECKS				10
+
+/*
+**
+*/
+
+typedef char	t_arg_type;
+
+#define T_REG					1 //0b0001
+#define T_DIR					2 //0b0010
+#define T_IND					4 //0b0100
+#define T_LAB					8 //0b1000
+
+/*
+**
+*/
+
+# define PROG_NAME_LENGTH		(128)
+# define COMMENT_LENGTH			(2048)
+# define COREWAR_EXEC_MAGIC		0xea83f3
+
+typedef struct		header_s
+{
+	unsigned int		magic;
+	char				prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int		prog_size;
+	char				comment[COMMENT_LENGTH + 1];
+}					header_t;
